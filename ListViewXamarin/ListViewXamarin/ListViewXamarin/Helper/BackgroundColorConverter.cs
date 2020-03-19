829dde693af9de5cbb063c49c849834be15f0348
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-03-19 09:56:35 +0530

    XAMARIN-20197 - Sample added

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/BackgroundColorConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class BackgroundColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var button = parameter as Button;
+
+            if ((bool?)value == null && button.Text == "Busy")
+                return Color.Red;
+            else if ((bool?)value == true && button.Text == "Available")
+                return Color.YellowGreen;
+            else if ((bool?)value == false && button.Text == "Away")
+                return Color.Orange;
+            else
+                return Color.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,59 @@
+﻿using Syncfusion.DataSource.Extensions;
+using Syncfusion.GridCommon.ScrollAxis;
+using Syncfusion.ListView.XForms;
+using Syncfusion.ListView.XForms.Control.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class Behavior : Behavior<Grid>
+    {
+        Button Button1;
+        Button Button2;
+        Button Button3;
+
+        protected override void OnAttachedTo(Grid bindable)
+        {
+            Button1 = bindable.FindByName<Button>("Button1");
+            Button2 = bindable.FindByName<Button>("Button2");
+            Button3 = bindable.FindByName<Button>("Button3");
+
+            Button1.Clicked += OnClicked;
+            Button2.Clicked += OnClicked;
+            Button3.Clicked += OnClicked;
+
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Grid bindable)
+        {
+            Button1.Clicked -= OnClicked;
+            Button2.Clicked -= OnClicked;
+            Button3.Clicked -= OnClicked;
+            Button1 = null;
+            Button2 = null;
+            Button3 = null;
+            base.OnDetachingFrom(bindable);
+        }
+
+        private void OnClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var bc = button.BindingContext as Contacts;
+
+            if (button.Text == "Available")
+                bc.Availability = true;
+            else if (button.Text == "Away")
+                bc.Availability = false;
+            else
+                bc.Availability = null;
+        }
+    }
+}
